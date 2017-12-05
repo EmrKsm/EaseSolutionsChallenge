6a776782b2cc2f43b13fbb2f72c841e236e3f44b
Ahmet Emir Kesim <ahmetemirkesim@gmail.com>
2017-12-05 14:35:12 +0300

    Recursive function changed. Program finds 1422 drop value for 1000*1000 map provided by Marcel.

--- EaseChallenge/EaseChallenge/Program.cs
+++ EaseChallenge/EaseChallenge/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,6 +14,8 @@ namespace EaseChallenge
         static int lengthOfPath = 1;
         static int dropOfPath = 0;
         static String choosenPath = String.Empty;
+        static String pathToPlayWith = String.Empty;
+        static String pathToPlayWithSubnodes = String.Empty;
 
         [STAThread]
         static void Main()
@@ -33,9 +36,18 @@ namespace EaseChallenge
                         //This list will contain possible paths that rider could choose
                         List<String> listOfPathsPossible = new List<string>();
                         lengthOfPath = 1;
+                        int progress = 0;
                         //Calculate paths row by row
                         for (int i = 0; i < lengthOfMap; i++)
                         {
+                            //Progress text for big scale maps. Added to show current progress of 1000*1000 map.
+                            if ((i % 10) == 0 && i!=0)
+                            {
+                                progress += 10;
+                                Console.Write("\r{0}%", progress/10);
+                                Thread.Sleep(40);
+                                Console.Clear();
+                            }
                             int[] row = Enumerable.Range(0, map.GetUpperBound(1) + 1).Select(x => map[i, x]).ToArray();
                             for (int j = 0; j < row.Length; j++)
                             {
@@ -60,6 +72,7 @@ namespace EaseChallenge
                                 choosenPath = path;
                             }
                         }
+                        Console.Clear();
                         Console.WriteLine(String.Format("Path: {0}\n\rLength: {1}\n\rDrop: {2}", choosenPath, lengthOfPath.ToString(),dropOfPath.ToString()));
                     }
                     catch (Exception ex)
@@ -79,7 +92,7 @@ namespace EaseChallenge
         /// Calculates drop of path between starting point and ending poinnt
         /// </summary>
         /// <param name="path">Path string</param>
-        /// <returns></returns>
+        /// <returns>Returns calculated drop value for given path</returns>
         static int CalculateDropOfPath(string path)
         {
             int firstPoint = Convert.ToInt32(path.Split('-')[0]);
@@ -92,23 +105,65 @@ namespace EaseChallenge
         /// <param name="values">Map array</param>
         /// <param name="selectedX">Current X coordinate points to look for next point</param>
         /// <param name="selectedY">Current Y coordinate points to look for next point</param>
-        /// <returns></returns>
+        /// <returns>Returns best calculated path for given location</returns>
         static string CreatePath(int[,] values, int selectedX, int selectedY)
         {
             int selectedPoint = values[selectedY, selectedX];
-            int currentPoint = selectedPoint;
             string jumpDirection = string.Empty;
-            Dictionary<string, int> neighboors = new Dictionary<string, int>();
+            Dictionary<string, int> neighbors = CreateNeighbors(values, selectedX, selectedY, selectedPoint);
+            List<String> pathsForSubnodes = new List<string>();
+            if (neighbors.Count <= 0)
+                return selectedPoint.ToString();
+            foreach (var neighbor in neighbors)
+            {
+                switch (neighbor.Key)
+                {
+                    case "north":
+                        pathToPlayWithSubnodes += selectedPoint.ToString() + "-" + CreatePath(values, selectedX, selectedY - 1);
+                        break;
+                    case "south":
+                        pathToPlayWithSubnodes += selectedPoint.ToString() + "-" + CreatePath(values, selectedX, selectedY + 1);
+                        break;
+                    case "east":
+                        pathToPlayWithSubnodes += selectedPoint.ToString() + "-" + CreatePath(values, selectedX + 1, selectedY);
+                        break;
+                    case "west":
+                        pathToPlayWithSubnodes += selectedPoint.ToString() + "-" + CreatePath(values, selectedX - 1, selectedY);
+                        break;
+                    default:
+                        pathToPlayWithSubnodes += selectedPoint.ToString();
+                        break;
+                }
+                pathsForSubnodes.Add(pathToPlayWithSubnodes);
+                pathToPlayWithSubnodes = String.Empty;
+            }
+            var maxLengthOfPaths = pathsForSubnodes.Max(p => p.Split('-').Length);
+            pathsForSubnodes.RemoveAll(p => p.Split('-').Length != maxLengthOfPaths);
+            var minOfPathsLastElement = pathsForSubnodes.Min(p => p.Split('-').Last());
+            pathToPlayWith = pathsForSubnodes.FirstOrDefault(p => p.Split('-').Length == maxLengthOfPaths && p.Split('-').Last() == minOfPathsLastElement);
+            return pathToPlayWith;  
+        }
+        /// <summary>
+        /// Create neighbors as key,value pair (key=direction, value=value) for given point 
+        /// </summary>
+        /// <param name="values">Map of points</param>
+        /// <param name="selectedX">Given point X</param>
+        /// <param name="selectedY">Given point Y</param>
+        /// <param name="selectedPoint">Given point's value</param>
+        /// <returns>Return list of neighbors for given location</returns>
+        private static Dictionary<string, int> CreateNeighbors(int[,] values, int selectedX, int selectedY, int selectedPoint)
+        {
+            Dictionary<string, int> neighbors = new Dictionary<string, int>();
             if (selectedX > 0)
-                neighboors.Add("west", values[selectedY , selectedX-1]);
-            if (selectedX < values.GetLength(1)-1)
-                neighboors.Add("east", values[selectedY, selectedX+1]);
+                neighbors.Add("west", values[selectedY, selectedX - 1]);
+            if (selectedX < values.GetLength(1) - 1)
+                neighbors.Add("east", values[selectedY, selectedX + 1]);
             if (selectedY > 0)
-                neighboors.Add("north", values[selectedY-1, selectedX ]);
+                neighbors.Add("north", values[selectedY - 1, selectedX]);
             if (selectedY < values.GetLength(0) - 1)
-                neighboors.Add("south", values[selectedY+1, selectedX]);
+                neighbors.Add("south", values[selectedY + 1, selectedX]);
             List<string> keysToRemove = new List<string>();
-            foreach (var point in neighboors)
+            foreach (var point in neighbors)
             {
                 if (point.Value >= selectedPoint)
                 {
@@ -117,36 +172,16 @@ namespace EaseChallenge
             }
             foreach (var key in keysToRemove)
             {
-                neighboors.Remove(key);
+                neighbors.Remove(key);
             }
 
-            if(neighboors.Count > 0)
-                if (neighboors.Values.Max() < selectedPoint )
-                    jumpDirection = neighboors.FirstOrDefault(x => x.Value.Equals(neighboors.Values.Max()) && x.Value < currentPoint).Key;
-
-            switch (jumpDirection)
-            {
-                case "north":
-                    neighboors.TryGetValue("north", out currentPoint);
-                    return selectedPoint.ToString() + "-" + CreatePath(values, selectedX, selectedY - 1);
-                case "south":
-                    neighboors.TryGetValue("south", out currentPoint);
-                    return selectedPoint.ToString() + "-" + CreatePath(values, selectedX, selectedY + 1);
-                case "east":
-                    neighboors.TryGetValue("east", out currentPoint);
-                    return selectedPoint.ToString() + "-" + CreatePath(values, selectedX + 1, selectedY);
-                case "west":
-                    neighboors.TryGetValue("west", out currentPoint);
-                    return selectedPoint.ToString() + "-" + CreatePath(values, selectedX - 1, selectedY);
-                default:
-                    return selectedPoint.ToString();
-            }
+            return neighbors;
         }
         /// <summary>
         /// Creates map from given file contains points. Get length of dimension from first line, other lines will be points
         /// </summary>
         /// <param name="path">File path contains points</param>
-        /// <returns></returns>
+        /// <returns>Returns points array with given file path</returns>
         static int[,] CreatePointsTableFromFile(string path)
         {
             try
